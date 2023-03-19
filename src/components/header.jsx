781f80f749a292da067e16741ddfaea14b28034c
michael <michaelutoh21@gmail.com>
2023-03-19 02:55:05 +0100

    Navbar icons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { CiSearch } from "react-icons/ci";
+import { TfiHelpAlt, TfiShoppingCart } from "react-icons/tfi";
+import { VscAccount } from "react-icons/vsc";
 
 
 const NavBar = () => {
   return (
     <nav className='flex items-center justify-between mx-auto p-2 w-10/12'>
-      <p className='font1 font-bold text-2xl'>SHOP</p>
+      <p className='font1 font-bold text-2xl'><Link to="/">Nazimo</Link></p>
 
       <div className='border border-gray-400 flex items-center justify-end p-2 rounded-full w-4/12'>
         <input className='search-box w-11/12' />
@@ -14,16 +16,16 @@ const NavBar = () => {
       </div>
   
       <ul className='flex font-thin'>
-        <li className='font2 ml-3'>
-          <Link to="/">Home</Link>
-        </li>
-        <li className='font2 ml-3'>
+        <li className='flex font2 items-center ml-5'>
+          <VscAccount className='mr-1' />
           <Link to="/account">Account</Link>
         </li>
-        <li className='font2 ml-3'>
+        <li className='flex font2 items-center ml-5'>
+          <TfiHelpAlt className='mr-1' />
           <Link to="/help">Help</Link>
         </li>
-        <li className='font2 ml-3'>
+        <li className='flex font2 items-center ml-5'>
+          <TfiShoppingCart className='mr-1 text-24' size={18} />
           <Link to="/cart">Cart</Link>
         </li>
       </ul>
